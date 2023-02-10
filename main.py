f835e5083a191495af9b4286a8f8bac178cfda3c
mikolajzendarski <mikolajzendarski@gmail.com>
2023-02-10 13:12:42 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle
+# Canvas
+myturtle = turtle.Turtle()
+
+# Start at given coordinates
+myturtle.up() # No marks
+myturtle.goto(54, 75)
+
+myturtle.pendown() # start marking
+# Movement of turtle
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+turtle.done()
